90d608a847cbb8ea97322d003b03f50febb99679
Thom-Mundstock <thomasmundstock@gmail.com>
2023-04-08 15:38:46 -0300

    validações categoria platina

--- Project/src/test/java/ContaMagicaTest.java
+++ Project/src/test/java/ContaMagicaTest.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContaMagicaTest {
@@ -5,4 +8,37 @@ class ContaMagicaTest {
     @org.junit.jupiter.api.Test
     void getNumeroConta() {
     }
+
+
+    @Test
+    public void verificacaoSaldo(){
+        ContaMagica conta = new ContaMagica("100445-14", "Thomas Mundstock");
+        conta.deposito(90);
+
+        Assertions.assertEquals(conta.retirada(100), false);
+    }
+
+    @Test
+    public void verificaPlatina(){
+        ContaMagica conta = new ContaMagica("100445-14", "Thomas Mundstock");
+        conta.deposito(50000);
+        conta.deposito(1000);
+        conta.deposito(20000000);
+
+        Assertions.assertEquals("PLATINUM", conta.getCategoria());
+    }
+
+    @Test
+    public void verificaValorizacaoPlatinum(){
+        ContaMagica conta = new ContaMagica("100445-14", "Thomas Mundstock");
+        conta.deposito(50000);
+        conta.deposito(200000);
+        conta.deposito(100);
+        conta.deposito(100);
+
+        Assertions.assertEquals(250203.5, conta.getSaldo());
+    }
+
+
+
 }
